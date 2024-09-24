168aaa26caa5b9e84b516c17fec4ed7768986fa7
SaifD227 <saifaalii237@gmail.com>
2024-09-24 12:25:25 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split vendor libraries into their own chunk
+          if (id.includes('node_modules')) {
+            return 'vendor'; // All node_modules go to 'vendor' chunk
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size limit as needed
+  },
+});
